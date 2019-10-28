660858ba0aa1cc60139b14cee4f76ac690fb7592
syedanaskamal <syedanaskamal@gmail.com>
2019-10-28 23:19:04 +0500

    changes to game

--- BESE Arcade/game/display.js
+++ BESE Arcade/game/display.js
@@ -1,44 +1,70 @@
-const Display = function(canvas){
+const Display = function(canvas) {
 
-    this.buffer = document.createElement("canvas").getContext("2d"),
+    this.buffer  = document.createElement("canvas").getContext("2d"),
     this.context = canvas.getContext("2d");
-
-    this.drawMap = function(image, image_columns, map, map_columns, tile_size) {
-
-        for (let index = map.length - 1; index > -1; --index){
-            let value = map[index];
-            let source_x = (value % image_columns) * tile_size;
-            let source_y = Math.floor(value / image_columns) * tile_size;
-            let destination_x = (index % map_columns ) * tile_size;
-            let destination_y = Math.floor(index / map_columns) * tile_size;
-
-            this.buffer.drawImage(image, source_x, source_y, tile_size, tile_size, destination_x, destination_y, tile_size, tile_size);
-
-        }
+  
+    this.tile_sheet = new Display.TileSheet(16, 8);
+  
+    /* This function draws the map to the buffer. */
+    this.drawMap = function(map, columns) {
+  
+      for (let index = map.length - 1; index > -1; -- index) {
+  
+        let value = map[index] - 1;
+        let source_x =           (value % this.tile_sheet.columns) * this.tile_sheet.tile_size;
+        let source_y = Math.floor(value / this.tile_sheet.columns) * this.tile_sheet.tile_size;
+        let destination_x =           (index % columns) * this.tile_sheet.tile_size;
+        let destination_y = Math.floor(index / columns) * this.tile_sheet.tile_size;
+  
+        this.buffer.drawImage(this.tile_sheet.image, source_x, source_y, this.tile_sheet.tile_size, this.tile_sheet.tile_size, destination_x, destination_y, this.tile_sheet.tile_size, this.tile_sheet.tile_size);
+  
+      }
+  
     };
-
-    this.drawObject = function(image, source_x, source_y, destination_x, destination_y, width, height) {
-        this.buffer.drawImage(image, source_x, source_y, width, height, Math.round(destination_x), Math.round(destination_y), width, height);
+  
+    this.drawPlayer = function(rectangle, color1, color2) {
+  
+      this.buffer.fillStyle = color1;
+      this.buffer.fillRect(Math.floor(rectangle.x), Math.floor(rectangle.y), rectangle.width, rectangle.height);
+      this.buffer.fillStyle = color2;
+      this.buffer.fillRect(Math.floor(rectangle.x + 2), Math.floor(rectangle.y + 2), rectangle.width - 4, rectangle.height - 4);
+  
     };
-
+  
     this.resize = function(width, height, height_width_ratio) {
-        if(height/width > height_width_ratio) {
-            this.context.canvas.height = width * height_width_ratio;
-            this.context.canvas.width = width;
-        } else {
-            this.context.canvas.height = height;
-            this.context.canvas.width = height/height_width_ratio;
-        }
-
-        this.context.imageSmoothingEnabled = false;
+  
+      if (height / width > height_width_ratio) {
+  
+        this.context.canvas.height = width * height_width_ratio;
+        this.context.canvas.width = width;
+  
+      } else {
+  
+        this.context.canvas.height = height;
+        this.context.canvas.width = height / height_width_ratio;
+  
+      }
+  
+      this.context.imageSmoothingEnabled = false;
+  
     };
-
-
-};
-
-Display.prototype = {
-    constructor: Display,
-
-    render:function(){ this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height); },
-
-};
+  
+  };
+  
+  Display.prototype = {
+  
+    constructor : Display,
+  
+    render:function() { this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height); },
+  
+  };
+  
+  Display.TileSheet = function(tile_size, columns) {
+  
+    this.image = new Image();
+    this.tile_size = tile_size;
+    this.columns = columns;
+  
+  };
+  
+  Display.TileSheet.prototype = {};
