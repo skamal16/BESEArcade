a4467cfffec4d60352a0f9fe9da171e4ab6a81f5
syedanaskamal <syedanaskamal@gmail.com>
2019-12-21 00:19:06 +0500

    A SHIT TON OF CHANGES

---
+++ BESE Arcade/pages/server_scripts/getUserDetails.php
@@ -0,0 +1,15 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', '', 'redemption');
+
+session_start();
+
+$user = $_SESSION['login'];
+
+$result = mysqli_query($db, "SELECT * FROM userscores WHERE username = '$user'") or die ("Failed");
+$row = mysqli_fetch_array($result);
+
+$score = $row["level1"] + $row["level2"] + $row["level3"] + $row["level4"] + $row["level5"] + $row["level6"];
+
+echo $user." has a total score of ".$score;
+?>
