7acf0315636f2fc3f51561e49652e2eb827eb4ba
ghazitiwana <35327403+ghazitiwana@users.noreply.github.com>
2019-12-22 22:50:28 +0500

    Logout time

--- BESE Arcade/pages/server_scripts/signOut.php
+++ BESE Arcade/pages/server_scripts/signOut.php
@@ -1,4 +1,8 @@
 <?php
     session_start();
+    $db = mysqli_connect('localhost', 'root', '', 'redemption');
+    $username=$_SESSION['login'];
+    $action="logout";
+    $query=mysqli_query($db,"insert into userlog(username,action) values('$username','$action')");
     unset($_SESSION["login"]);
 ?>
