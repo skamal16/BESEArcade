fc0c2c3b96627ac9728b751ca5edfe267f42f1d0
armeen3 <57043186+armeen3@users.noreply.github.com>
2019-12-23 22:27:20 +0500

    Update googlelogin.php

--- BESE Arcade/pages/googlelogin.php
+++ BESE Arcade/pages/googlelogin.php
@@ -10,6 +10,7 @@ session_start();
 	$_SESSION['login']=$user;
 	mysqli_query($db,"insert into users(username) values('$user')");
 	mysqli_query($db,"insert into userlog(username, action) values('$user', 'login')");
+	mysqli_query($db, "INSERT INTO userscores (username, level1, level2, level3, level4, level5, level6) VALUES ('$user', 0, 0, 0, 0, 0, 0);");
 	echo "$user";
 
 ?>
