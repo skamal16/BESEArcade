1c234596c89f5ea585f4f87eb207a6645d3da7a2
armeen3 <57043186+armeen3@users.noreply.github.com>
2019-12-23 21:47:30 +0500

    I HELPED

--- BESE Arcade/pages/googlelogin.php
+++ BESE Arcade/pages/googlelogin.php
@@ -8,7 +8,7 @@ $db = mysqli_connect('localhost', 'root', '', 'redemption');
 
 session_start();
 	$_SESSION['login']=$user;
-	mysqli_query($db,"insert into users(username) values('$user'");
+	mysqli_query($db,"insert into users(username) values('$user')");
 	mysqli_query($db,"insert into userlog(username, action) values('$user', 'login')");
 	echo "$user";
 
