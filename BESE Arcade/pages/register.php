6b5fa247fd4b6e6d020874fa0a2896bbb649565b
ghazitiwana <35327403+ghazitiwana@users.noreply.github.com>
2019-12-24 15:04:34 +0500

    Final touches

--- BESE Arcade/pages/register.php
+++ BESE Arcade/pages/register.php
@@ -11,15 +11,21 @@ $pass = mysqli_real_escape_string($db, $_POST['pass']);
 $username = mysqli_real_escape_string($db, $_POST['username']);
 
 $password = md5($pass);
-if(mysqli_query($db, "CALL RegisterUser('$username', '$fname', '$email', $contact, '$password')")){
-    $_SESSION['login']=$username;
 
-    $cookie_name = "login_details";
+if(mysqli_query($db, "CALL RegisterUser('$username', '$fname', '$email', $contact, '$password')")){
+
+	$result = mysqli_query($db, "CALL LoginUser('$username', '$password', @success)") or die("DATABASE ERROR");
+	$result = mysqli_fetch_assoc($result);
+
+	$_SESSION['login']=$username;
+
+	$cookie_name = "login_details";
 	$cookie_value = '{ "username":"'.$username.'", "password":"'.$pass.'"}';
 
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-	
-    echo "success";
-} else echo mysqli_error($db);
+
+	echo "success";
+
+	} else echo "failed";
 
 ?>
